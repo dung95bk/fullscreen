c96713d25acc703246314e0d0238e62024ada75a
dung95bk <tiendungpham95@gmail.com>
2023-01-16 20:35:39 +0700

    hide status bar

--- app/src/main/java/com/example/fullscreen/MainActivity.kt
+++ app/src/main/java/com/example/fullscreen/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.fullscreen
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.widget.Toast
 import com.example.fullscreen.databinding.ActivityMainBinding
 
@@ -16,6 +20,20 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnFullStatusbar.setOnClickListener {
             startActivity(Intent(this, Fullstatusbar::class.java))
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+            window.insetsController?.systemBarsBehavior =
+                WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        } else {
+            val flags = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+            window.decorView.systemUiVisibility = flags
         }
     }
 
