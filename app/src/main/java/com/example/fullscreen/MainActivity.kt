1137523dc29605ab712be9ead68cd97a8fd455dc
dung95bk <tiendungpham95@gmail.com>
2024-09-14 19:36:17 +0700

    add

--- app/src/main/java/com/example/fullscreen/MainActivity.kt
+++ app/src/main/java/com/example/fullscreen/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.fullscreen
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fullscreen.databinding.ActivityMainBinding
+import eightbitlab.com.blurview.BlurAlgorithm
+import eightbitlab.com.blurview.RenderEffectBlur
+import eightbitlab.com.blurview.RenderScriptBlur
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +21,17 @@ class MainActivity : AppCompatActivity() {
         binding.btnFullScreen.setOnClickListener {
             startActivity(Intent(this, FullscreenActivity::class.java))
         }
+        //set background, if your root layout doesn't have one
+
+        //set background, if your root layout doesn't have one
+        val windowBackground = window.decorView.background
+        val algorithm: BlurAlgorithm = getBlurAlgorithm()
         binding.btnFullStatusbar.setOnClickListener {
             startActivity(Intent(this, Fullstatusbar::class.java))
+        }
+        binding.apply {
+            blurView.setupWith(root, RenderScriptBlur(this@MainActivity))
+                .setBlurRadius(2f)
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
@@ -37,7 +49,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun getBlurAlgorithm(): BlurAlgorithm {
+        val algorithm: BlurAlgorithm = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            RenderEffectBlur()
+        } else {
+            RenderScriptBlur(this)
+        }
+        return algorithm
+    }
     override fun onBackPressed() {
-        CustomToast.makeText(this, "dsadsada", Toast.LENGTH_LONG, R.layout.custom_toast_finish_warning).show()
+        CustomToast.makeText(
+            this,
+            "dsadsada",
+            Toast.LENGTH_LONG,
+            R.layout.custom_toast_finish_warning
+        ).show()
     }
 }
